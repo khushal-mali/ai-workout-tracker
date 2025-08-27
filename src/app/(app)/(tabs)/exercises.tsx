88d8ce78eed4a452ec3541d03a71927c2480eeb1
Khushal Mali <khushalmali8448@gmail.com>
2025-08-27 10:37:12 +0530

    exercises page

--- src/app/(app)/(tabs)/exercises.tsx
+++ src/app/(app)/(tabs)/exercises.tsx
@@ -1,11 +1,125 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import { useEffect, useState } from "react";
+import {
+  FlatList,
+  RefreshControl,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { defineQuery } from "groq";
+import { client } from "@/lib/sanity/client";
+import { Exercise } from "@/lib/sanity/types";
+import ExerciseCard from "@/components/ExerciseCard";
+
+export const exercisesQuery = defineQuery(`*[_type == "exercise"] {
+    ...
+  }`);
 
 const Exercises = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [refreshing, setRefreshing] = useState(false);
+  const [exercises, setExercises] = useState<Exercise[]>([]);
+  const [filteredExercises, setFilteredExercises] = useState([]);
+  const router = useRouter();
+
+  const fetchExercises = async () => {
+    try {
+      // Fetch exercises from sanity
+      const exercises = await client.fetch(exercisesQuery);
+
+      console.log(exercises);
+
+      setExercises(exercises);
+      setFilteredExercises(exercises);
+    } catch (error) {
+      console.log("Error fetching exercises:", error);
+    }
+    return;
+  };
+
+  useEffect(() => {
+    fetchExercises();
+  }, []);
+
+  useEffect(() => {
+    const filtered = exercises.filter((exercise) =>
+      exercise.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredExercises(filtered);
+  }, [searchQuery, exercises]);
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await fetchExercises();
+    setRefreshing(false);
+  };
+
   return (
-    <SafeAreaView className="flex-1">
-      <Text>Exercises</Text>
+    <SafeAreaView className="flex-1 bg-gray-50">
+      {/* Header */}
+      <View className="px-6 py-4 bg-white border-b border-gray-200">
+        <Text className="text-2xl font-bold text-gray-900">Exercise Library</Text>
+        <Text className="text-gray-600 mt-1">Discover and master new exercise</Text>
+
+        {/* Search Bar */}
+        <View className="flex-row items-center bg-gray-100 rounded-xl px-3 py-1 mt-4">
+          <Ionicons name="search" size={20} color={"#6B7280"} />
+          <TextInput
+            className="flex-1 ml-3 text-gray-800"
+            placeholder="Search exercise..."
+            placeholderTextColor={"#9CA3AF"}
+            value={searchQuery}
+            onChangeText={setSearchQuery}
+          />
+
+          {searchQuery.length > 0 && (
+            <TouchableOpacity onPress={() => setSearchQuery("")}>
+              <Ionicons name="close-circle" size={20} color={"#6B7280"} />
+            </TouchableOpacity>
+          )}
+        </View>
+      </View>
+
+      {/* Exercise Grid */}
+      <FlatList
+        data={filteredExercises}
+        keyExtractor={(item) => item._id}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ padding: 24 }}
+        renderItem={({ item }) => (
+          <ExerciseCard
+            item={item}
+            onPress={() => router.push(`/exercise-datail?id=${item._id}`)}
+          />
+        )}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            colors={["#3B82F6"]} // Android
+            tintColor={"#3B82F6"} // iOS
+            title="Pull to refresh exercises" // iOS
+            titleColor={"#6B7280"}
+          />
+        }
+        ListEmptyComponent={
+          <View className="bg-white rounded-2xl p-8 items-center">
+            <Ionicons name="fitness-outline" size={64} color={"#9ca3af"} />
+            <Text className="text-xl font-semibold text-gray-900 mt-4">
+              {searchQuery ? "No exercises found." : "Loading exercises..."}
+            </Text>
+            <Text className="text-gray-600 text-center mt-2">
+              {searchQuery
+                ? "Try adjusting your search"
+                : "Your exercises will appear here."}
+            </Text>
+          </View>
+        }
+      />
     </SafeAreaView>
   );
 };
