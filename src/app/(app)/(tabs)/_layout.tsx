dc87a38cc5704989dfd8c52ff4d4e09c38c73331
Khushal Mali <khushalmali8448@gmail.com>
2025-08-31 13:39:35 +0530

    user image added on navlink

--- src/app/(app)/(tabs)/_layout.tsx
+++ src/app/(app)/(tabs)/_layout.tsx
@@ -1,9 +1,12 @@
+import { useUser } from "@clerk/clerk-expo";
+import AntDesign from "@expo/vector-icons/AntDesign";
 import { Tabs } from "expo-router";
 import React from "react";
-import AntDesign from "@expo/vector-icons/AntDesign";
 import { Image } from "react-native";
 
 const Layout = () => {
+  const { user } = useUser();
+
   return (
     <Tabs screenOptions={{ headerShown: false }}>
       <Tabs.Screen
@@ -63,10 +66,12 @@ const Layout = () => {
         options={{
           headerShown: false,
           title: "Profile",
-          // href: null,
           tabBarIcon: ({ color, size }) => (
-            // <Image source={}/>
-            <AntDesign name="user" color={color} size={size} />
+            <Image
+              source={{ uri: user?.imageUrl ?? user?.externalAccounts[0]?.imageUrl }}
+              className="rounded-full"
+              style={{ width: 28, height: 28, borderRadius: 100 }}
+            />
           ),
         }}
       />
